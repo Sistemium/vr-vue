77b68b44545959bf22a58d144ab212fec740b405
Alexander Levin <alevin.ru@gmail.com>
2018-09-29 22:25:33 +0300

    style init

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,28 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+<div id="app">
+  <div id="nav">
+    <router-link to="/">Home</router-link>
+    |
+    <router-link to="/about">About</router-link>
   </div>
+  <router-view/>
+</div>
 </template>
 
 <style lang="scss">
+
+body {
+  background: rgb(243, 244, 246);
+}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
 }
+
 #nav {
   padding: 30px;
   a {
@@ -26,4 +33,5 @@
     }
   }
 }
+
 </style>
